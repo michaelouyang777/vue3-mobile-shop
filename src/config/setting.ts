b2e4ac6338553ed8187a722901280539458d7a6b
BNDXQC\ouyangxintang <oy.xt@qq.com>
2021-01-27 12:55:44 +0800

    调整配置

---
+++ src/config/setting.ts
@@ -0,0 +1,3 @@
+module.exports = {
+  title: 'app-mobile-shop'
+}
--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,29 @@
+'use strict'
 const path = require('path')
 const resolve = dir => path.join(__dirname, dir)
 
 const ProgressBarPlugin = require('progress-bar-webpack-plugin')
 const chalk = require('chalk')
 
-const Timestamp = new Date().getTime()
+const timestamp = new Date().getTime()
 const isNotDevelopment = process.env.NODE_ENV !== 'dev'
+const defaultSettings = require('./src/config/setting.ts')
 
 module.exports = {
   outputDir: process.env.NODE_ENV,
   publicPath: './',
+  assetsDir: 'static',
   lintOnSave: false,
   productionSourceMap: false,
-  chainWebpack: config => {
+  configureWebpack(config) {
+    config.name = defaultSettings.title
+    config.output.filename = `js/[name].[hash].${timestamp}.js`
+    config.output.chunkFilename = `js/[name].[hash].${timestamp}.js`
+  },
+  chainWebpack(config) {
+    config.resolve.alias.set('@', resolve('src'))
+
+    // set svg-sprite-loader
     config.module
       .rule('svg')
       .exclude.add(resolve('src/assets/icons'))
@@ -28,12 +39,14 @@ module.exports = {
         symbolId: 'icon-[name]'
       })
       .end()
+
     // isNotDevelopment
     if (isNotDevelopment) {
       config.optimization.minimize(true)
       config.optimization.splitChunks({
         chunks: 'all'
       })
+      // shutdown preload
       config.plugins.delete('preload')
       config.plugins.delete('prefetch')
       config.module
@@ -48,9 +61,5 @@ module.exports = {
         )
       config.plugin('progressBarPlugin').use(new ProgressBarPlugin())
     }
-  },
-  configureWebpack: config => {
-    config.output.filename = `js/[name].[hash].${Timestamp}.js`
-    config.output.chunkFilename = `js/[name].[hash].${Timestamp}.js`
   }
 }
